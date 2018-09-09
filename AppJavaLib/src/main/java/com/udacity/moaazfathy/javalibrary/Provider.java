71c6734cbddf0d70cb7bb19f486f9107466e340b
sobhy mohamed mohamed elsayed <30624911+elbhwashy@users.noreply.github.com>
2018-09-09 14:04:21 +0200

    first commit

---
+++ AppJavaLib/src/main/java/com/udacity/moaazfathy/javalibrary/Provider.java
@@ -0,0 +1,13 @@
+package com.udacity.moaazfathy.javalibrary;
+
+public class Provider {
+    String joke;
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+
+    public String getJoke(){
+
+        return "joke";
+    }}
